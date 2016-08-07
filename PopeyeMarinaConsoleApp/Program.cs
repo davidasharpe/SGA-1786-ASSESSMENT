2575d52ebf18c76c300ee6149c3f2a3946923aaf
David Sharpe <davidasharpe@me.com>
2016-08-07 14:42:05 +1000

    work in progress

---
+++ PopeyeMarinaConsoleApp/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PopeyeMarinaConsoleApp
+{
+    public class Customer
+    {
+        public int customerID { get; set; }
+        public string customerName { get; set; }
+        public string customerAddress { get; set; }
+        public int customerPhoneNo { get; set; }
+
+        public Customer(int ID, string Name, string Address, int Phone)
+        {
+            ID = customerID;
+            Name = customerName;
+            Address = customerAddress;
+            Phone = customerPhoneNo;
+        }
+    }
+    
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Customer customer1 = new Customer(1, "David Sharpe", "13-38 Hampden Road Artmarmon", 0431677215);
+
+            Console.WriteLine("Customer ID = {0} Name = {1} Address = {2} Phone = {3}",
+                customer1.customerID,
+                customer1.customerName,
+                customer1.customerAddress,
+                customer1.customerPhoneNo);
+
+            Console.ReadLine();
+        }
+    }
+}
+
+
